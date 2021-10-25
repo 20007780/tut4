3d035df63fac08ec8195da6ecca082ce935b1e0b
CoCo <736401991@qq.com>
2021-10-25 17:47:42 +0800

    First commit

---
+++ Calc.java
@@ -0,0 +1,16 @@
+public class Calc 
+{
+    public static void main( String[] args )
+    {
+        public int add{
+            public int a;
+            a = (int)(Math.random() * 10) + (int)(Math.random() * 10);
+            return a;
+        }
+         public int subtract{
+            public int b;
+            b = (int)(Math.random() * 10) + + (int)(Math.random() * 10);
+            return b;
+        }
+        
+}
